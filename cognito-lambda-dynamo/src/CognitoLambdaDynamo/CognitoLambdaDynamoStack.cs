838e2f90fa8e2940cb4277469efd256db97472d9
Niki Oro <niki_oro@hotmail.it>
2021-12-19 12:31:36 +0100

    cognito-lambda-dynamo

---
+++ cognito-lambda-dynamo/src/CognitoLambdaDynamo/CognitoLambdaDynamoStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CognitoLambdaDynamo
+{
+    public class CognitoLambdaDynamoStack : Stack
+    {
+        internal CognitoLambdaDynamoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
