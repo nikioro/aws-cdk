12908a92fd120b9a0d22e6b2a0d2fbcf60d22238
Niki Oro <niki_oro@hotmail.it>
2021-12-19 18:36:21 +0100

    added simple lambda

--- cognito-lambda-dynamo/src/CognitoLambdaDynamo/CognitoLambdaDynamoStack.cs
+++ cognito-lambda-dynamo/src/CognitoLambdaDynamo/CognitoLambdaDynamoStack.cs
@@ -1,4 +1,5 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.IAM;
 using Amazon.CDK.AWS.Lambda;
 using Constructs;
 
@@ -8,12 +9,19 @@ namespace CognitoLambdaDynamo
     {
         internal CognitoLambdaDynamoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            Function fn = new Function(this, "MyFunction", new FunctionProps
+        //    var myRole =   new Role(this, "My Cdk Role" , new RoleProps{ AssumedBy = new ServicePrincipal("sns.amazonaws.com") });
+            Function fn = new Function(this, "My CDK Function", new FunctionProps
             {
                 Runtime = Runtime.DOTNET_CORE_3_1,
-                Handler = "index.handler",
-                Code = Code.FromAsset(string.Join("", "lambda-handler"))
+                Handler = "FunctionHandler",
+                Code = Code.FromAsset(
+                    "src\\CognitoLambdaDynamo\\lambda-handler"),
+                // Role =myRole
             });
+
+            // myRole.AddManagedPolicy(ManagedPolicy.FromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
+            // myRole.AddManagedPolicy(ManagedPolicy.FromAwsManagedPolicyName("service-role/AWSLambdaVPCAccessExecutionRole")); // only required if your function lives in a VPC
+
         }
     }
 }
